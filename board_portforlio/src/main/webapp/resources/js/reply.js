150e7679aebac07a8acaad94f70bb781c03bcce1
atomsk <bigman01kr@gmail.com>
2018-11-01 12:07:27 +0900

    comment CRUD

--- board_portforlio/src/main/webapp/resources/js/reply.js
+++ board_portforlio/src/main/webapp/resources/js/reply.js
@@ -40,8 +40,9 @@ var replyService = (function(){
 		});
 	}
 	
-	function remove(reply, callback, error){
+	function remove(rno, callback, error){
 		
+
 		$.ajax({
 			type : 'delete',
 			url : '/replies/' + rno,
@@ -71,19 +72,58 @@ var replyService = (function(){
 					callback(result);
 				}
 			},
-			error : fuction(xhr, status, er){
+			error : function(xhr, status, er){
 				if(error){
 					error(er);
 				}
 			}
 		});
+	};
+	
+	function get(rno, callback, error){
+		$.get("/replies/" + rno + ".json", function(result){
+			if(callback){
+				callback(result);
+			}
+		}).fail(function(xhr, status, err){
+			if (error){
+				error();
+			}
+		});
 	}
+	
+	function displayTime(timeValue){
+		var today = new Date();
+		var gap = today.getTime() - timeValue;
+		var dateObj = new Date(timeValue);
+		var str = "";
+		
+		if (gap < (1000 * 60 * 60 * 24)) {
+			var hh = dateObj.getHours();
+			var mi = dateObj.getMinutes();
+			var ss = dateObj.getSeconds();
+			
+			return [(hh>9 ? '' : '0') + hh, ':', (mi>9 ? '':'0') + mi,
+				':', (ss>9 ? '' : '0') + ss].join('');
+			
+			
+		}else{
+			var yy = dataObj.getFullyear();
+			var mm = dateObj.getMonth() + 1;
+			var dd = dateObj.getDate();
+			
+			return[yy, '/', (mm>9 ? '' : '0')+ mm, '/',
+				(dd>9 ? '' : '0')+ dd].join('');
+		}
+	};
 	
 	
 	return{
 		add:add,
 		getList:getList,
 		remove:remove,
-		update:update
+		update:update,
+		get : get,
+		displayTime:displayTime
 	};
 })();
