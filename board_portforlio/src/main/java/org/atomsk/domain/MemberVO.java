d1e368d8f99b8fe798c0700137d926a8f1eec6d8
atomsk <bigman01kr@gmail.com>
2018-11-14 20:57:12 +0900

    login custom user detail

--- board_portforlio/src/main/java/org/atomsk/domain/MemberVO.java
+++ board_portforlio/src/main/java/org/atomsk/domain/MemberVO.java
@@ -1,13 +1,16 @@
 package org.atomsk.domain;
 
 import java.util.Date;
+import java.util.List;
 
 import lombok.Data;
 
 @Data
 public class MemberVO {
-	private Integer uenabled;
-	private String userid, userpw, unick, urole;
-	private Date uregdate, uupdatedate;
+	private boolean enabled;
+	private String userid, userpw, userName;
+	private Date regDate, updateDate;
+	private List<AuthVO> authList;
+	
 
 }
---
+++ board_portforlio/src/main/java/org/atomsk/mapper/MemberMapper.java
@@ -0,0 +1,9 @@
+package org.atomsk.mapper;
+
+import org.atomsk.domain.MemberVO;
+
+public interface MemberMapper {
+	
+	public MemberVO read(String userid);
+
+}
